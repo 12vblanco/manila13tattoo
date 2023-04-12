3af7770ba69299ef9969b13cf79b0a65022afb97
Victor Blanco <12vblanco@gmail.com>
2023-04-13 00:54:58 +0800

    deleteSvg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
 function App() {
-  return <>Coming Soon!!</>;
+  return (
+    <Div>
+      <h1>Manila 13 Tattoo</h1>
+      <h2>Coming Soon!!</h2>
+    </Div>
+  );
 }
+
+const Div = styled.div`
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default App;
