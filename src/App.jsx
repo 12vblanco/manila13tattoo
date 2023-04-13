c9a9370e98f39b0af6eea173cb4b64cdb42e9874
Victor Blanco <12vblanco@gmail.com>
2023-04-13 11:16:47 +0800

    comingSoon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import img1 from "./assets/pic1.png";
+import img2 from "./assets/pic2.png";
+import img3 from "./assets/pic3.png";
+import img4 from "./assets/pic4.png";
 
 function App() {
   return (
     <Div>
       <h1>Manila 13 Tattoo</h1>
+      <PicDiv>
+        {" "}
+        <Img src={img1} />
+        <Img src={img2} />
+        <Img src={img3} />
+        <Img src={img4} />
+      </PicDiv>
       <h2>Coming Soon!!</h2>
     </Div>
   );
@@ -17,6 +28,20 @@ const Div = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const PicDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Img = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 6px;
+  margin: 12px;
 `;
 
 export default App;
